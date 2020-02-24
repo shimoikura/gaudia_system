781f33a30976850982e5aed25fbc7f1d879a1d51
ikura <ikura554@gmail.com>
2020-02-24 14:57:20 +0900

    [テスト]ルーティングの設定変更

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return 'welcome to test page!';
+});
+
+Route::get('/students', 'StudentController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

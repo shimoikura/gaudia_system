d47eb402a7a69e8ef01fc7c61c5b7bdcd8d0e14b
root <root@localhost.localdomain>
2020-02-24 11:44:43 +0900

    Databaseの作成

---
+++ database/migrations/2020_02_23_082220_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('gender');
+            $table->string('grade');
+            $table->string('role');
+            $table->string('id_condition')->nullable();
+            $table->string('id_class');
+            $table->string('updated_at');
+            $table->string('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
